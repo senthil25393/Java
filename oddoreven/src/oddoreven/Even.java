ca4137f4382a9da3dc99ca5d0f30b69951d8c8d6
senthil25393 <senthilshyam25@gmail.com>
2022-05-29 21:51:30 +0530

    Java commit

---
+++ oddoreven/src/oddoreven/Even.java
@@ -0,0 +1,17 @@
+package oddoreven;
+
+public class Even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=10;
+if (i==10) {
+	
+	System.out.println("Even");
+} else {
+
+	System.out.println("odd");
+}
+	}
+
+}
